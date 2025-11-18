8454cd1d033f696df798d3d9c802fdd086ab7e20
Junzhe <jshi70@jh.edu>
2025-11-18 03:05:09 -0500

    Finish receiver demo

--- connection.cpp
+++ connection.cpp
@@ -6,41 +6,133 @@
 #include "connection.h"
 
 Connection::Connection()
-  : m_fd(-1)
-  , m_last_result(SUCCESS) {
+    : m_fd(-1), m_last_result(SUCCESS)
+{
 }
 
 Connection::Connection(int fd)
-  : m_fd(fd)
-  , m_last_result(SUCCESS) {
+    : m_fd(fd), m_last_result(SUCCESS)
+{
   // TODO: call rio_readinitb to initialize the rio_t object
+  rio_readinitb(&m_fdbuf, m_fd);
 }
 
-void Connection::connect(const std::string &hostname, int port) {
+void Connection::connect(const std::string &hostname, int port)
+{
   // TODO: call open_clientfd to connect to the server
+  std::string port_str = std::to_string(port);
+  m_fd = open_clientfd(hostname.c_str(), port_str.c_str());
   // TODO: call rio_readinitb to initialize the rio_t object
+  if (m_fd >= 0)
+  {
+    rio_readinitb(&m_fdbuf, m_fd);
+  }
 }
 
-Connection::~Connection() {
+Connection::~Connection()
+{
   // TODO: close the socket if it is open
+  if (is_open())
+  {
+    close();
+  }
 }
 
-bool Connection::is_open() const {
+bool Connection::is_open() const
+{
   // TODO: return true if the connection is open
+  return m_fd >= 0;
 }
 
-void Connection::close() {
+void Connection::close()
+{
   // TODO: close the connection if it is open
+  if (is_open())
+  {
+    Close(m_fd);
+    m_fd = -1;
+  }
 }
 
-bool Connection::send(const Message &msg) {
+bool Connection::send(const Message &msg)
+{
   // TODO: send a message
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+  if (!is_open())
+  {
+    m_last_result = EOF_OR_ERROR;
+    return false;
+  }
+  std::string formatted_msg = msg.tag + ":" + msg.data + "\n";
+
+  // Check length of message
+  if (formatted_msg.length() > Message::MAX_LEN)
+  {
+    m_last_result = INVALID_MSG;
+    return false;
+  }
+
+  // Send Message
+  ssize_t byte_msg = rio_writen(m_fd, formatted_msg.c_str(), formatted_msg.length());
+
+  // Check successfully send all content
+  if (byte_msg != (ssize_t)formatted_msg.length())
+  {
+    m_last_result = EOF_OR_ERROR;
+    return false;
+  }
+
+  m_last_result = SUCCESS;
+  return true;
 }
 
-bool Connection::receive(Message &msg) {
+bool Connection::receive(Message &msg)
+{
   // TODO: receive a message, storing its tag and data in msg
   // return true if successful, false if not
   // make sure that m_last_result is set appropriately
+  if (!is_open())
+  {
+    m_last_result = EOF_OR_ERROR;
+    return false;
+  }
+
+  // Read one line from the connection to get the message sent
+  char msg_buffer[Message::MAX_LEN + 1];
+  ssize_t byte_msg_r = rio_readlineb(&m_fdbuf, msg_buffer, Message::MAX_LEN);
+
+  // Check content read successfully
+  if (byte_msg_r <= 0)
+  {
+    m_last_result = EOF_OR_ERROR;
+    return false;
+  }
+
+  // Extracting message
+  // Convert to string and remove the trailing \n
+  std::string line(msg_buffer);
+  if (!line.empty() && (line.back() == '\n' || line.back() == '\r'))
+  {
+    line.pop_back();
+    if (!line.empty() && line.back() == '\r')
+    {
+      line.pop_back();
+    }
+  }
+
+  // Find the colon separator for body and tag extraction
+  size_t colon_position = line.find(':');
+  if (colon_position >= line.length())
+  {
+    m_last_result = INVALID_MSG;
+    return false;
+  }
+
+  // Extract
+  msg.tag = line.substr(0, colon_position);
+  msg.data = line.substr(colon_position + 1);
+
+  m_last_result = SUCCESS;
+  return true;
 }
--- receiver.cpp
+++ receiver.cpp
@@ -7,8 +7,10 @@
 #include "connection.h"
 #include "client_util.h"
 
-int main(int argc, char **argv) {
-  if (argc != 5) {
+int main(int argc, char **argv)
+{
+  if (argc != 5)
+  {
     std::cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
     return 1;
   }
@@ -21,13 +23,109 @@ int main(int argc, char **argv) {
   Connection conn;
 
   // TODO: connect to server
+  try
+  {
+    conn.connect(server_hostname, server_port);
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "Failed to connect to server\n";
+    return 1;
+  }
+
+  if (!conn.is_open())
+  {
+    std::cerr << "Failed to connect to server\n";
+    return 1;
+  }
 
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
 
+  // Send rlogin message
+  Message rlogin_msg(TAG_RLOGIN, username); // initialize a login message
+  if (!conn.send(rlogin_msg))
+  {
+    std::cerr << "Failed to send rlogin message\n";
+    return 1;
+  }
+
+  Message response; // store the response from sending message
+  if (!conn.receive(response))
+  {
+    std::cerr << "Failed to receive rlogin message\n";
+    return 1;
+  }
+
+  if (response.tag == TAG_ERR)
+  {
+    std::cerr << response.data << std::endl;
+    return 1;
+  }
+
+  // Send join message
+  Message join_msg(TAG_JOIN, room_name);
+  if (!conn.send(join_msg))
+  {
+    std::cerr << "Failed to send join message\n";
+    return 1;
+  }
+
+  // Wait for response to join
+  if (!conn.receive(response))
+  {
+    std::cerr << "Failed to receive join response\n";
+    return 1;
+  }
+
+  if (response.tag == TAG_ERR)
+  {
+    std::cerr << response.data << std::endl;
+    return 1;
+  }
+
+  if (response.tag == TAG_ERR)
+  {
+    std::cerr << response.data << std::endl;
+    return 1;
+  }
+
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
+  while (true)
+  {
+    Message incoming_msg;
+    if (!conn.receive(incoming_msg))
+    {
+      std::cerr << "Failed to receive message from server\n";
+      return 1;
+    }
 
+    // Validate the message here
+    if (incoming_msg.tag == TAG_DELIVERY)
+    {
+      // Format check and message parsing
+      size_t first_colon = incoming_msg.data.find(":");
+      if (first_colon > incoming_msg.data.length())
+      {
+        std::cerr << "Invalid message format received\n";
+        continue; // skip to next iteration
+      }
 
+      size_t second_colon = incoming_msg.data.find(":", first_colon + 1);
+      if (second_colon > incoming_msg.data.length())
+      {
+        std::cerr << "Invalid message format received\n";
+        continue; // skip to next iteration
+      }
+
+      // Extract sender and message text
+      std::string sender = incoming_msg.data.substr(first_colon + 1, second_colon - first_colon - 1);
+      std::string message_text = incoming_msg.data.substr(second_colon + 1);
+
+      // Display message in required format: [sender]: message_text
+      std::cout << sender << ": " << message_text << std::endl;
+    }
+  }
   return 0;
 }
